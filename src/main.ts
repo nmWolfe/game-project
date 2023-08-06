83224c3ad55c34b874e03fc1cd5b6c2d102a5fb5
Nick <n.m.phelan@gmail.com>
2023-08-06 12:06:15 +0100

    Adding all to switch branches

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-import "./styles.scss";
+import "./main.scss";
+
+const directionLeft = document.querySelector("#arrow-left");
+const directionRight = document.querySelector("#arrow-right");
+const directionUp = document.querySelector("#arrow-up");
+const directionDown = document.querySelector("#arrow-down");
+
+if (!directionDown || !directionLeft || !directionUp || !directionRight) {
+  throw new Error("Directional var error");
+}
