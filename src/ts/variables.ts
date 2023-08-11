ef3ed2ae79ba60758bf54483541753efdc431ef1
Nick <n.m.phelan@gmail.com>
2023-08-11 07:42:15 +0100

    Fixed file path for img again

--- src/ts/variables.ts
+++ src/ts/variables.ts
@@ -9,5 +9,5 @@ Press enter to continue.
 (you can also click on the d-pad)
 </p>`;
 // Creating SuperMazeBoy Icon and Corn icon
-export const playerIcon = `<img src="../assets/logo.svg" alt="super maize boy" width="40%" height="90%"/>`;
-export const cornIcon = `<img src="../assets/corn.svg" alt="corn icon" width="40%" height="90%"/>`;
+export const playerIcon = `<img src="./src/assets/logo.svg" alt="super maize boy" width="40%" height="90%"/>`;
+export const cornIcon = `<img src="./src/assets/corn.svg" alt="corn icon" width="40%" height="90%"/>`;
