a48947bad8cac97252fcc36852b3d9f7d7586997
Nick <n.m.phelan@gmail.com>
2023-08-11 12:54:07 +0100

    Added functionality for resetting game after victory, and display window for win

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { rules } from "./ts/variables";
 import { handleEnterStart, handleClickStart } from "./ts/events";
 
 export let rulesDisplayed: boolean = false;
-// Creating SuperMazeBoy Icon and Corn icon
-export const playerIcon = `<img src="../assets/logo.svg" alt="super maize boy" width="40%" height="90%"/>`;
-export const cornIcon = `<img src="../assets/corn.svg" alt="corn icon" width="40%" height="90%"/>`;
 
 // Display rules on page load
 const handleRules = () => {
